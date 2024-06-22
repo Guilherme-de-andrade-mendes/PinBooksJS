2f099ca7e8c462cb62ab1d254d32fdde77ec1bc2
GuiAMendes <114957916+GuiAMendes@users.noreply.github.com>
2024-06-22 16:15:45 -0300

    feat: implementation of function 'delete_people' for deleting objects in array 'peoples'

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
-const peoples =  JSON.parse(localStorage.getItem('Peoples')) || [];
+let peoples = JSON.parse(localStorage.getItem('Peoples')) || [];
 
- //Função para limpar o terminal de visualizações;
- function cleaner(){
+// Função para limpar o conteúdo da área de visualização.
+function cleaner() {
     const view = document.getElementsByClassName('view')[0];
     view.innerHTML = '';
-};
-
-/*A função itera por meio de 'some' para analisar se pelo menos um dos elementos passa na condição da função, tendo em vista que o CPF que é um chave-primária, logo, não pode haver duplicidade de valores;*/
-function exist_CPF(parameter){
-    let exist = peoples.some(values => values.CPF === parameter);
-    return exist
 }
 
-/*A função itera por meio de 'some' para analisar se pelo menos um dos elementos passa na condição da função, tendo em vista que o CPF que é um chave-primária, logo, não pode haver duplicidade de valores.
-Caso encontre o CPf retorna o valor de sua posição no array 'peoples', do contrário, devolve um valor negativo;*/
-function find_people(parameter){
+// Função que verifica se um determinado CPF já existe na lista de pessoas.
+// Utiliza o método 'some' para iterar pelos elementos e verificar duplicidade.
+function exist_CPF(parameter) {
+    let exist = peoples.some(values => values.CPF === parameter);
+    return exist;
+}
+
+// Função que busca uma pessoa pelo CPF e retorna o índice na lista de pessoas.
+// Utiliza o método 'some' para iterar pelos elementos. Retorna -1 se não encontrado.
+function find_people(parameter) {
     let index = -1;
     peoples.some((values, i) => {
-        if(values.CPF === parameter){
+        if (values.CPF === parameter) {
             index = i;
         }
     });
-    return (index === -1? -1 : index);
+    return index;
 }
 
-//Função responsavel por alimentar o array de pessoas, contendo os atributos de cada usuário no sistema;
-function add_peoples(){
-    //Declaração das variaveis que recebem os parâmetros de entrada dos inputs;
+// Função responsável por adicionar uma nova pessoa à lista.
+// Os atributos da pessoa são obtidos dos inputs do formulário.
+function add_peoples() {
     let CPF = document.getElementById('textCPF').value;
     let name = document.getElementById('textName').value;
     let street = document.getElementById('textStreet').value;
@@ -37,7 +38,6 @@ function add_peoples(){
     let birthday = document.getElementById('textBirthday').value;
     let profession = document.getElementById('textProfession').value;
 
-    //Construção do Object que receberá os valores de entrada contidos anteriormente;
     const p = {
         CPF,
         name,
@@ -50,97 +50,93 @@ function add_peoples(){
         profession
     };
     const view = document.getElementsByClassName('view')[0];
-    try{
+    try {
         let match = exist_CPF(CPF);
-    
-        /*Bloco de verificação do elemento, onde , utiliza do retorno da iteração anterior para fazer inclusão ou emitir uma mensagem ao usuário que o CPF passado já existe na base de dados; */
-        if(!match){
+
+        // Verifica se o CPF já existe. Se não, adiciona a nova pessoa à lista e salva os dados.
+        if (!match) {
             peoples.push(p);
-            saveData();// Grava os dados no LocalStorage do navegador, permitindo assim a permanência das informações;
-            view.innerHTML =  "Pessoa inserida com sucesso!";
-        }else
+            saveData_peoples();
+            view.innerHTML = "Pessoa inserida com sucesso!";
+        } else {
             view.innerHTML = "Esse CPF já está em uso. Tente novamente com um novo!";
-    }catch(e){
+        }
+    } catch (e) {
         console.log("Não foi possível inserir ou verificar o CPF do usuário.");
-        console.log(e.name); // Imprimi o nome do erro no console;
-        console.log(e.message); //Imprimi a mensagem do erro no console;
-    }finally{
-        //A função setTimeout faz com que a função de limpeza do terminal ative apenas uma vez depois 5000 milísegundos;
+        console.log(e.name); // Imprime o nome do erro no console.
+        console.log(e.message); // Imprime a mensagem do erro no console.
+    } finally {
+        // Limpa a área de visualização após 5000 milissegundos.
         setTimeout(cleaner, 5000);
     }
 }
 
-function findOne_people(){
+// Função que busca e exibe uma pessoa pelo CPF fornecido.
+function findOne_people() {
     let CPF = document.getElementById('textCPF').value;
-    try{
-        /*Faz a primeira busca para ver se consegue encontrar a pessoa com o CPF informado pelo valor de entrada que será utilizado como parâmetro da função 'find_people';*/
+    try {
         let match = find_people(CPF);
 
-        /*Se o retorno da função for um valor for maior que -1 a função continua, do contrário, ele emite uma mensagem para o usuário informando que não foi possivel encontrar
-        o usuário do CPF informado;*/
-        if(match > -1){
-            //Cria dinâmicamento elementos para armazenar os dados que serão recuperados do array;
+        if (match > -1) {
             const view = document.getElementsByClassName('view')[0];
-            view.textContent= '';
+            view.textContent = '';
             const attributsPeople = document.createElement('p');
-            
-            //Declara uma variavel com o índice do CPF informado, permitindo assim a recuperação dos dados;
+
             const person = peoples[match];
-            attributsPeople.textContent = `CPF: ${person.CPF}\nNome: ${person.name}\nEndereço: ${person.street}, ${person.nro}\nCEP: ${person.CEP}\nEmails: ${'[' + person.emails.join(', ') + ' ]'}\nTelefones: ${'[' + person.telephones.join(', ') + ' ]'}\nAniversário: ${person.birthday}\nProfissão: ${person.profession}`;
+            attributsPeople.textContent = `CPF: ${person.CPF}\nNome: ${person.name}\nEndereço: ${person.street}, ${person.nro}\nCEP: ${person.CEP}\nEmails: [${person.emails.join(', ')}]\nTelefones: [${person.telephones.join(', ')}]\nAniversário: ${person.birthday}\nProfissão: ${person.profession}`;
             view.appendChild(attributsPeople);
-        }else
+        } else {
             view.innerHTML = 'O CPF informado não consta na base de dados atual.';
-    }catch(e){
+        }
+    } catch (e) {
         console.log("Ocorreu um erro ao tentar buscar o usuário.");
-        console.log(e.name); // Imprimi o nome do erro no console;
-        console.log(e.message); //Imprimi a mensagem do erro no console;
-    }finally{
-        //A função setTimeout faz com que a função de limpeza do terminal ative apenas uma vez depois 5000 milísegundos;
+        console.log(e.name); // Imprime o nome do erro no console.
+        console.log(e.message); // Imprime a mensagem do erro no console.
+    } finally {
+        // Limpa a área de visualização após 45000 milissegundos.
         setTimeout(cleaner, 45000);
     }
 }
 
-function saveData_peoples(){
-    localStorage.setItem('Peoples', JSON.stringify(peoples)); // Utilza o LocalStorage do navegador para armazenar os dados do array "peoples", transformando inicialmente em string JSON;
+// Função que salva a lista de pessoas no LocalStorage.
+function saveData_peoples() {
+    localStorage.setItem('Peoples', JSON.stringify(peoples));
 }
 
-function findAll_peoples(){
+// Função que exibe todas as pessoas armazenadas na lista.
+function findAll_peoples() {
     const view = document.getElementsByClassName('view')[0];
     view.textContent = '';
-    try{
-        if(peoples !== null){
+    try {
+        if (peoples.length > 0) {
             peoples.forEach(element => {
                 let attributsPeople = document.createElement('p');
-                attributsPeople.textContent = `CPF: ${element.CPF}\nNome: ${element.name}\nEndereço: ${element.street}, ${element.nro}\nCEP: ${element.CEP}\nEmails: ${'[' + element.emails.join(', ') + ' ]'}\nTelefones: ${'[' + element.telephones.join(', ') + ' ]'}\nAniversário: ${element.birthday}\nProfissão: ${element.profession}`;
+                attributsPeople.textContent = `CPF: ${element.CPF}\nNome: ${element.name}\nEndereço: ${element.street}, ${element.nro}\nCEP: ${element.CEP}\nEmails: [${element.emails.join(', ')}]\nTelefones: [${element.telephones.join(', ')}]\nAniversário: ${element.birthday}\nProfissão: ${element.profession}`;
                 view.appendChild(attributsPeople);
             });
-        }else{
+        } else {
             view.innerHTML = 'Não existem dados para serem recuperados. Insira alguns e tente novamente.';
         }
-    }catch(e){
+    } catch (e) {
         console.log("Ocorreu um erro ao tentar buscar os dados dos usuários.");
-        console.log(e.name); // Imprimi o nome do erro no console;
-        console.log(e.message); //Imprimi a mensagem do erro no console;
-    }finally{
-        //A função setTimeout faz com que a função de limpeza do terminal ative apenas uma vez depois 5000 milísegundos;
+        console.log(e.name); // Imprime o nome do erro no console.
+        console.log(e.message); // Imprime a mensagem do erro no console.
+    } finally {
+        // Limpa a área de visualização após 45000 milissegundos.
         setTimeout(cleaner, 45000);
     }
 }
 
-function update_people(){
+// Função que atualiza os dados de uma pessoa existente na lista.
+function update_people() {
     let CPF = document.getElementById('textCPF').value;
-    try{
-        /*Faz a primeira busca para ver se consegue encontrar a pessoa com o CPF informado pelo valor de entrada que será utilizado como parâmetro da função 'find_people';*/
+    try {
         let match = find_people(CPF);
 
-        /*Se o retorno da função for um valor for maior que -1 a função continua, do contrário, ele emite uma mensagem para o usuário informando que não foi possivel encontrar
-        o usuário do CPF informado;*/
-        if(match > -1){
-            //Cria dinâmicamento elementos para armazenar os dados que serão recuperados do array;
+        if (match > -1) {
             const view = document.getElementsByClassName('view')[0];
-            view.textContent= '';
-            
-            //Declara uma variavel com o índice do CPF informado, permitindo assim a alteração dos dados;
+            view.textContent = '';
+
             const person = peoples[match];
             person.name = document.getElementById('textName').value;
             person.street = document.getElementById('textStreet').value;
@@ -151,15 +147,42 @@ function update_people(){
             person.birthday = document.getElementById('textBirthday').value;
             person.profession = document.getElementById('textProfession').value;
             view.textContent = 'Os dados do usuário foram atualizados com sucesso!';
-        }else
+        } else {
             view.innerHTML = 'O CPF informado não consta na base de dados atual.';
-    }catch(e){
-        console.log("Ocorreu um erro ao tentar atualizar os dados do usuário específicado.");
-        console.log(e.name); // Imprimi o nome do erro no console;
-        console.log(e.message); //Imprimi a mensagem do erro no console;
-    }finally{
-        //A função setTimeout faz com que a função de limpeza do terminal ative apenas uma vez depois 5000 milísegundos;
+        }
+    } catch (e) {
+        console.log("Ocorreu um erro ao tentar atualizar os dados do usuário especificado.");
+        console.log(e.name); // Imprime o nome do erro no console.
+        console.log(e.message); // Imprime a mensagem do erro no console.
+    } finally {
+        // Limpa a área de visualização após 45000 milissegundos.
         setTimeout(cleaner, 45000);
     }
 }
 
+// Função que exclui uma pessoa da lista pelo CPF.
+function delete_people() {
+    let CPF = document.getElementById('textCPF').value;
+    try {
+        let match = find_people(CPF);
+
+        if (match > -1) {
+            const view = document.getElementsByClassName('view')[0];
+            view.textContent = '';
+
+            // Remove a pessoa da lista filtrando pelo CPF.
+            peoples = peoples.filter(element => element.CPF !== CPF);
+            saveData_peoples();
+            view.textContent = 'O usuário foi excluído com sucesso!';
+        } else {
+            view.innerHTML = 'O CPF informado não consta na base de dados atual.';
+        }
+    } catch (e) {
+        console.log("Ocorreu um erro ao tentar excluir os dados do usuário especificado.");
+        console.log(e.name); // Imprime o nome do erro no console.
+        console.log(e.message); // Imprime a mensagem do erro no console.
+    } finally {
+        // Limpa a área de visualização após 45000 milissegundos.
+        setTimeout(cleaner, 45000);
+    }
+}
