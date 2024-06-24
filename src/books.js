d9352d26aeb93decec1bd07a283d60ac65d089e3
maria-monteiro <maria.camara@aluno.ifsp.edu.br>
2024-06-24 19:03:30 -0300

    feat: Add function to check existence of ISBN

--- src/books.js
+++ src/books.js
@@ -11,3 +11,11 @@ function cleaner() {
     const view = document.getElementsByClassName('view')[0];
     view.innerHTML = '';
 }
+
+// Função que verifica se um determinado ISBN já existe na lista de livros.
+// Utiliza o método 'some' para iterar pelos elementos e verificar duplicidade.
+function exist_ISBN(parameter) {
+    return books.some(values => values.ISBN === parameter);
+}
+
+
