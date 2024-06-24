cef80cdc41d9a993de0888a654a973eebcbf50c7
GuiAMendes <114957916+GuiAMendes@users.noreply.github.com>
2024-06-24 20:31:25 -0300

    feat: add new elements in struct HTML

--- src/books.js
+++ src/books.js
@@ -1,168 +1,172 @@
-let books = JSON.parse(localStorage.getItem('Books')) || [];
-
+let books = JSON.parse(localStorage.getItem("Books")) || [];
 
 // Função que salva a lista de livros no LocalStorage.
 function saveData_books() {
-    localStorage.setItem('Books', JSON.stringify(books));
+  localStorage.setItem("Books", JSON.stringify(books));
 }
 
 // Função para limpar o conteúdo da área de visualização.
 function cleaner() {
-    const view = document.getElementsByClassName('view')[0];
-    view.innerHTML = '';
+  const view = document.getElementsByClassName("view")[0];
+  view.innerHTML = "";
 }
 
 // Função que verifica se um determinado ISBN já existe na lista de livros.
 // Utiliza o método 'some' para iterar pelos elementos e verificar duplicidade.
 function exist_ISBN(parameter) {
-    return books.some(values => values.ISBN === parameter);
+  return books.some((values) => values.ISBN === parameter);
 }
 
 // Função que busca um livro pelo ISBN e retorna o índice na lista de livros.
 // Utiliza o método 'findIndex' para iterar pelos elementos. Retorna -1 se não encontrado.
 function find_book(parameter) {
-    return books.findIndex(values => values.ISBN === parameter);
+  return books.findIndex((values) => values.ISBN === parameter);
 }
 
 // Função responsável por adicionar um novo livro à lista.
 // Os atributos do livro são obtidos dos inputs do formulário.
 function add_books() {
-	let ISBN = document.getElementById('textISBN').value;
-	let title = document.getElementById('textTitle').value;
-	let author = document.getElementById('textAuthor').value;
-	let year = document.getElementById('textYear').value;
-	let genre = document.getElementById('textGenre').value;
-	let quantity = document.getElementById('textQuantity').value;
+  let ISBN = document.getElementById("textISBN").value;
+  let title = document.getElementById("textTitle").value;
+  let author = document.getElementById("textAuthor").value.split(",");
+  let year = document.getElementById("textYear").value;
+  let genre = document.getElementById("textGenre").value;
+  let quantity = document.getElementById("textQuantity").value;
 
-	const b = {
-    	ISBN,
-    	title,
-    	author,
-    	year,
-    	genre,
-    	quantity
-	};
+  const b = {
+    ISBN,
+    title,
+    author,
+    year,
+    genre,
+    quantity,
+  };
 
-	const view = document.getElementsByClassName('view')[0];
+  const view = document.getElementsByClassName("view")[0];
 
-	try {
-    	if (!exist_ISBN(ISBN)) {
-        	books.push(b);
-        	saveData_books();
-        	view.innerHTML = "Livro inserido com sucesso!";
-    	} else {
-        	view.innerHTML = "Esse ISBN já está em uso. Tente novamente com um novo!";
-    	}
-	} catch (e) {
-    	console.error("Não foi possível inserir ou verificar o ISBN do livro.", e);
-	} finally {
-    	setTimeout(cleaner, 5000);
-	}
+  try {
+    if (!exist_ISBN(ISBN)) {
+      books.push(b);
+      saveData_books();
+      view.innerHTML = "Livro inserido com sucesso!";
+    } else {
+      view.innerHTML = "Esse ISBN já está em uso. Tente novamente com um novo!";
+    }
+  } catch (e) {
+    console.error("Não foi possível inserir ou verificar o ISBN do livro.", e);
+  } finally {
+    setTimeout(cleaner, 5000);
+  }
 }
 
 // Função que busca e exibe um livro pelo ISBN fornecido.
 function findOne_book() {
-	let ISBN = document.getElementById('textISBN').value;
-	const view = document.getElementsByClassName('view')[0];
+  let ISBN = document.getElementById("textISBN").value;
+  const view = document.getElementsByClassName("view")[0];
 
-	try {
-    	let match = find_book(ISBN);
-    	if (match > -1) {
-        	const book = books[match];
-        	view.innerHTML = `
+  try {
+    let match = find_book(ISBN);
+    if (match > -1) {
+      const book = books[match];
+      view.innerHTML = `
             	<p>ISBN: ${book.ISBN}</p>
             	<p>Título: ${book.title}</p>
-            	<p>Autor: ${book.author}</p>
+            	<p>Autor(es): [ ${element.author.join(", ")} ]</p>
             	<p>Ano de Publicação: ${book.year}</p>
             	<p>Gênero: ${book.genre}</p>
             	<p>Quantidade: ${book.quantity}</p>
         	`;
-    	} else {
-        	view.innerHTML = 'O ISBN informado não consta na base de dados atual.';
-    	}
-	} catch (e) {
-    	console.error("Ocorreu um erro ao tentar buscar o livro.", e);
-	} finally {
-    	setTimeout(cleaner, 45000);
-	}
+    } else {
+      view.innerHTML = "O ISBN informado não consta na base de dados atual.";
+    }
+  } catch (e) {
+    console.error("Ocorreu um erro ao tentar buscar o livro.", e);
+  } finally {
+    setTimeout(cleaner, 45000);
+  }
 }
 
 // Função que exibe todos os livros armazenados na lista.
 function findAll_books() {
-	const view = document.getElementsByClassName('view')[0];
-	view.textContent = '';
+  const view = document.getElementsByClassName("view")[0];
+  view.textContent = "";
 
-	try {
-    	if (books.length > 0) {
-        	books.forEach(element => {
-            	const attributsBook = document.createElement('p');
-            	attributsBook.innerHTML = `
+  try {
+    if (books.length > 0) {
+      books.forEach((element) => {
+        const attributsBook = document.createElement("p");
+        attributsBook.innerHTML = `
                 	<p>ISBN: ${element.ISBN}</p>
                 	<p>Título: ${element.title}</p>
-                	<p>Autor: ${element.author}</p>
+                	<p>Autor(es): [ ${element.author.join(", ")} ]</p>
                 	<p>Ano de Publicação: ${element.year}</p>
                 	<p>Gênero: ${element.genre}</p>
                 	<p>Quantidade: ${element.quantity}</p>
                 	<br>
             	`;
-            	view.appendChild(attributsBook);
-        	});
-    	} else {
-        	view.innerHTML = 'Não existem dados para serem recuperados. Insira alguns e tente novamente.';
-    	}
-	} catch (e) {
-    	console.error("Ocorreu um erro ao tentar buscar os dados dos livros.", e);
-	} finally {
-    	setTimeout(cleaner, 45000);
-	}
+        view.appendChild(attributsBook);
+      });
+    } else {
+      view.innerHTML =
+        "Não existem dados para serem recuperados. Insira alguns e tente novamente.";
+    }
+  } catch (e) {
+    console.error("Ocorreu um erro ao tentar buscar os dados dos livros.", e);
+  } finally {
+    setTimeout(cleaner, 45000);
+  }
 }
 
 // Função que atualiza os dados de um livro existente na lista.
 function update_book() {
-	let ISBN = document.getElementById('textISBN').value;
-	const view = document.getElementsByClassName('view')[0];
+  let ISBN = document.getElementById("textISBN").value;
+  const view = document.getElementsByClassName("view")[0];
 
-	try {
-    	let match = find_book(ISBN);
-    	if (match > -1) {
-        	const book = books[match];
-        	book.title = document.getElementById('textTitle').value;
-        	book.author = document.getElementById('textAuthor').value;
-        	book.year = document.getElementById('textYear').value;
-        	book.genre = document.getElementById('textGenre').value;
-        	book.quantity = document.getElementById('textQuantity').value;
+  try {
+    let match = find_book(ISBN);
+    if (match > -1) {
+      const book = books[match];
+      book.title = document.getElementById("textTitle").value;
+      book.author = document.getElementById("textAuthor").value;
+      book.year = document.getElementById("textYear").value;
+      book.genre = document.getElementById("textGenre").value;
+      book.quantity = document.getElementById("textQuantity").value;
 
-        	saveData_books();
-        	view.innerHTML = 'Os dados do livro foram atualizados com sucesso!';
-    	} else {
-        	view.innerHTML = 'O ISBN informado não consta na base de dados atual.';
-    	}
-	} catch (e) {
-    	console.error("Ocorreu um erro ao tentar atualizar os dados do livro especificado.", e);
-	} finally {
-    	setTimeout(cleaner, 45000);
-	}
+      saveData_books();
+      view.innerHTML = "Os dados do livro foram atualizados com sucesso!";
+    } else {
+      view.innerHTML = "O ISBN informado não consta na base de dados atual.";
+    }
+  } catch (e) {
+    console.error(
+      "Ocorreu um erro ao tentar atualizar os dados do livro especificado.",
+      e
+    );
+  } finally {
+    setTimeout(cleaner, 45000);
+  }
 }
 
 // Função que exclui um livro da lista pelo ISBN.
 function delete_book() {
-	let ISBN = document.getElementById('textISBN').value;
-	const view = document.getElementsByClassName('view')[0];
+  let ISBN = document.getElementById("textISBN").value;
+  const view = document.getElementsByClassName("view")[0];
 
-	try {
-    	let match = find_book(ISBN);
-    	if (match > -1) {
-        	books = books.filter(element => element.ISBN !== ISBN);
-        	saveData_books();
-        	view.innerHTML = 'O livro foi excluído com sucesso!';
-    	} else {
-        	view.innerHTML = 'O ISBN informado não consta na base de dados atual.';
-    	}
-	} catch (e) {
-    	console.error("Ocorreu um erro ao tentar excluir os dados do livro especificado.", e);
-	} finally {
-    	setTimeout(cleaner, 45000);
-	}
+  try {
+    let match = find_book(ISBN);
+    if (match > -1) {
+      books = books.filter((element) => element.ISBN !== ISBN);
+      saveData_books();
+      view.innerHTML = "O livro foi excluído com sucesso!";
+    } else {
+      view.innerHTML = "O ISBN informado não consta na base de dados atual.";
+    }
+  } catch (e) {
+    console.error(
+      "Ocorreu um erro ao tentar excluir os dados do livro especificado.",
+      e
+    );
+  } finally {
+    setTimeout(cleaner, 45000);
+  }
 }
-
-
--- src/clients.js
+++ src/clients.js
@@ -1,190 +1,216 @@
-let peoples = JSON.parse(localStorage.getItem('Peoples')) || [];
-
+let peoples = JSON.parse(localStorage.getItem("Peoples")) || [];
 
 // Função para limpar o conteúdo da área de visualização.
 function cleaner() {
-    const view = document.getElementsByClassName('view')[0];
-    view.innerHTML = '';
+  const view = document.getElementsByClassName("view")[0];
+  view.innerHTML = "";
 }
 
 // Função que verifica se um determinado CPF já existe na lista de pessoas.
 // Utiliza o método 'some' para iterar pelos elementos e verificar duplicidade.
 function exist_CPF(parameter) {
-    let exist = peoples.some(values => values.CPF === parameter);
-    return exist;
+  let exist = peoples.some((values) => values.CPF === parameter);
+  return exist;
 }
 
 // Função que busca uma pessoa pelo CPF e retorna o índice na lista de pessoas.
 // Utiliza o método 'some' para iterar pelos elementos. Retorna -1 se não encontrado.
 function find_people(parameter) {
-    let index = -1;
-    peoples.some((values, i) => {
-        if (values.CPF === parameter) {
-            index = i;
-        }
-    });
-    return index;
+  let index = -1;
+  peoples.some((values, i) => {
+    if (values.CPF === parameter) {
+      index = i;
+    }
+  });
+  return index;
 }
 
 // Função responsável por adicionar uma nova pessoa à lista.
 // Os atributos da pessoa são obtidos dos inputs do formulário.
 function add_peoples() {
-    let CPF = document.getElementById('textCPF').value;
-    let name = document.getElementById('textName').value;
-    let street = document.getElementById('textStreet').value;
-    let nro = document.getElementById('textNumber').value;
-    let CEP = document.getElementById('textCEP').value;
-    let emails = document.getElementById('textEmails').value.split(",");
-    let telephones = document.getElementById('textTelephone_numbers').value.split(",");
-    let birthday = document.getElementById('textBirthday').value;
-    let profession = document.getElementById('textProfession').value;
+  let CPF = document.getElementById("textCPF").value;
+  let name = document.getElementById("textName").value;
+  let street = document.getElementById("textStreet").value;
+  let nro = document.getElementById("textNumber").value;
+  let CEP = document.getElementById("textCEP").value;
+  let emails = document.getElementById("textEmails").value.split(",");
+  let telephones = document
+    .getElementById("textTelephone_numbers")
+    .value.split(",");
+  let birthday = document.getElementById("textBirthday").value;
+  let profession = document.getElementById("textProfession").value;
 
-    const p = {
-        CPF,
-        name,
-        street,
-        nro,
-        CEP,
-        emails,
-        telephones,
-        birthday,
-        profession
-    };
-    const view = document.getElementsByClassName('view')[0];
-    try {
-        let match = exist_CPF(CPF);
+  const p = {
+    CPF,
+    name,
+    street,
+    nro,
+    CEP,
+    emails,
+    telephones,
+    birthday,
+    profession,
+  };
+  const view = document.getElementsByClassName("view")[0];
+  try {
+    let match = exist_CPF(CPF);
 
-        // Verifica se o CPF já existe. Se não, adiciona a nova pessoa à lista e salva os dados.
-        if (!match) {
-            peoples.push(p);
-            saveData_peoples();
-            view.innerHTML = "Pessoa inserida com sucesso!";
-        } else {
-            view.innerHTML = "Esse CPF já está em uso. Tente novamente com um novo!";
-        }
-    } catch (e) {
-        console.log("Não foi possível inserir ou verificar o CPF do usuário.");
-        console.log(e.name); // Imprime o nome do erro no console.
-        console.log(e.message); // Imprime a mensagem do erro no console.
-    } finally {
-        // Limpa a área de visualização após 5000 milissegundos.
-        setTimeout(cleaner, 5000);
+    // Verifica se o CPF já existe. Se não, adiciona a nova pessoa à lista e salva os dados.
+    if (!match) {
+      peoples.push(p);
+      saveData_peoples();
+      view.innerHTML = "Pessoa inserida com sucesso!";
+    } else {
+      view.innerHTML = "Esse CPF já está em uso. Tente novamente com um novo!";
     }
+  } catch (e) {
+    console.log("Não foi possível inserir ou verificar o CPF do usuário.");
+    console.log(e.name); // Imprime o nome do erro no console.
+    console.log(e.message); // Imprime a mensagem do erro no console.
+  } finally {
+    // Limpa a área de visualização após 5000 milissegundos.
+    setTimeout(cleaner, 5000);
+  }
 }
 
 // Função que busca e exibe uma pessoa pelo CPF fornecido.
 function findOne_people() {
-    let CPF = document.getElementById('textCPF').value;
-    try {
-        let match = find_people(CPF);
+  let CPF = document.getElementById("textCPF").value;
+  try {
+    let match = find_people(CPF);
 
-        if (match > -1) {
-            const view = document.getElementsByClassName('view')[0];
-            view.textContent = '';
-            const attributsPeople = document.createElement('p');
+    if (match > -1) {
+      const view = document.getElementsByClassName("view")[0];
+      view.textContent = "";
+      const attributsPeople = document.createElement("p");
 
-            const person = peoples[match];
-            attributsPeople.textContent = `CPF: ${person.CPF}\nNome: ${person.name}\nEndereço: ${person.street}, ${person.nro}\nCEP: ${person.CEP}\nEmails: [${person.emails.join(', ')}]\nTelefones: [${person.telephones.join(', ')}]\nAniversário: ${person.birthday}\nProfissão: ${person.profession}`;
-            view.appendChild(attributsPeople);
-        } else {
-            view.innerHTML = 'O CPF informado não consta na base de dados atual.';
-        }
-    } catch (e) {
-        console.log("Ocorreu um erro ao tentar buscar o usuário.");
-        console.log(e.name); // Imprime o nome do erro no console.
-        console.log(e.message); // Imprime a mensagem do erro no console.
-    } finally {
-        // Limpa a área de visualização após 45000 milissegundos.
-        setTimeout(cleaner, 45000);
+      const person = peoples[match];
+      attributsPeople.innerHTML = `
+      <p>CPF: ${person.CPF}</p>
+      <p>Nome: ${person.name}</p>
+      <p>Endereço: ${person.street}</p>
+      <p>Numero${person.nro}</p>
+      <p>CEP: ${person.CEP}</p>
+      <p>Emails: [${person.emails.join(", ")}]</p>
+      <p>Telefones: [${person.telephones.join(", ")}]</p>
+      <p>Aniversário: ${person.birthday}</p>
+      <>Profissão: ${person.profession}</p>`;
+      view.appendChild(attributsPeople);
+    } else {
+      view.innerHTML = "O CPF informado não consta na base de dados atual.";
     }
+  } catch (e) {
+    console.log("Ocorreu um erro ao tentar buscar o usuário.");
+    console.log(e.name); // Imprime o nome do erro no console.
+    console.log(e.message); // Imprime a mensagem do erro no console.
+  } finally {
+    // Limpa a área de visualização após 45000 milissegundos.
+    setTimeout(cleaner, 45000);
+  }
 }
 
 // Função que salva a lista de pessoas no LocalStorage.
 function saveData_peoples() {
-    localStorage.setItem('Peoples', JSON.stringify(peoples));
+  localStorage.setItem("Peoples", JSON.stringify(peoples));
 }
 
 // Função que exibe todas as pessoas armazenadas na lista.
 function findAll_peoples() {
-    const view = document.getElementsByClassName('view')[0];
-    view.textContent = '';
-    try {
-        if (peoples.length > 0) {
-            peoples.forEach(element => {
-                let attributsPeople = document.createElement('p');
-                // attributsPeople.textContent = `CPF: ${element.CPF}\nNome: ${element.name}\nEndereço: ${element.street}, ${element.nro}\nCEP: ${element.CEP}\nEmails: [${element.emails.join(', ')}]\nTelefones: [${element.telephones.join(', ')}]\nAniversário: ${element.birthday}\nProfissão: ${element.profession}`;
-                attributsPeople.innerHTML = `CPF: ${element.CPF}\nNome: ${element.name}\nEndereço: ${element.street}, ${element.nro}\nCEP: ${element.CEP}\nEmails: [${element.emails.join(', ')}]\nTelefones: [${element.telephones.join(', ')}]\nAniversário: ${element.birthday}\nProfissão: ${element.profession}<br>`;
-                view.appendChild(attributsPeople);
-            });
-        } else {
-            view.innerHTML = 'Não existem dados para serem recuperados. Insira alguns e tente novamente.';
-        }
-    } catch (e) {
-        console.log("Ocorreu um erro ao tentar buscar os dados dos usuários.");
-        console.log(e.name); // Imprime o nome do erro no console.
-        console.log(e.message); // Imprime a mensagem do erro no console.
-    } finally {
-        // Limpa a área de visualização após 45000 milissegundos.
-        setTimeout(cleaner, 45000);
+  const view = document.getElementsByClassName("view")[0];
+  view.textContent = "";
+  try {
+    if (peoples.length > 0) {
+      peoples.forEach((element) => {
+        let attributsPeople = document.createElement("p");
+        // attributsPeople.textContent = `CPF: ${element.CPF}\nNome: ${element.name}\nEndereço: ${element.street}, ${element.nro}\nCEP: ${element.CEP}\nEmails: [${element.emails.join(', ')}]\nTelefones: [${element.telephones.join(', ')}]\nAniversário: ${element.birthday}\nProfissão: ${element.profession}`;
+        attributsPeople.innerHTML = `
+        <p>CPF: ${person.CPF}</p>
+        <p>Nome: ${person.name}</p>
+        <p>Endereço: ${person.street}</p>
+        <p>Numero${person.nro}</p>
+        <p>CEP: ${person.CEP}</p>
+        <p>Emails: [${person.emails.join(", ")}]</p>
+        <p>Telefones: [${person.telephones.join(", ")}]</p>
+        <p>Aniversário: ${person.birthday}</p>
+        <p>Profissão: ${person.profession}</p>`;
+        view.appendChild(attributsPeople);
+      });
+    } else {
+      view.innerHTML =
+        "Não existem dados para serem recuperados. Insira alguns e tente novamente.";
     }
+  } catch (e) {
+    console.log("Ocorreu um erro ao tentar buscar os dados dos usuários.");
+    console.log(e.name); // Imprime o nome do erro no console.
+    console.log(e.message); // Imprime a mensagem do erro no console.
+  } finally {
+    // Limpa a área de visualização após 45000 milissegundos.
+    setTimeout(cleaner, 45000);
+  }
 }
 
 // Função que atualiza os dados de uma pessoa existente na lista.
 function update_people() {
-    let CPF = document.getElementById('textCPF').value;
-    try {
-        let match = find_people(CPF);
+  let CPF = document.getElementById("textCPF").value;
+  try {
+    let match = find_people(CPF);
 
-        if (match > -1) {
-            const view = document.getElementsByClassName('view')[0];
-            view.textContent = '';
+    if (match > -1) {
+      const view = document.getElementsByClassName("view")[0];
+      view.textContent = "";
 
-            const person = peoples[match];
-            person.name = document.getElementById('textName').value;
-            person.street = document.getElementById('textStreet').value;
-            person.nro = document.getElementById('textNumber').value;
-            person.CEP = document.getElementById('textCEP').value;
-            person.emails = document.getElementById('textEmails').value.split(",");
-            person.telephones = document.getElementById('textTelephone_numbers').value.split(",");
-            person.birthday = document.getElementById('textBirthday').value;
-            person.profession = document.getElementById('textProfession').value;
-            view.textContent = 'Os dados do usuário foram atualizados com sucesso!';
-        } else {
-            view.innerHTML = 'O CPF informado não consta na base de dados atual.';
-        }
-    } catch (e) {
-        console.log("Ocorreu um erro ao tentar atualizar os dados do usuário especificado.");
-        console.log(e.name); // Imprime o nome do erro no console.
-        console.log(e.message); // Imprime a mensagem do erro no console.
-    } finally {
-        // Limpa a área de visualização após 45000 milissegundos.
-        setTimeout(cleaner, 45000);
+      const person = peoples[match];
+      person.name = document.getElementById("textName").value;
+      person.street = document.getElementById("textStreet").value;
+      person.nro = document.getElementById("textNumber").value;
+      person.CEP = document.getElementById("textCEP").value;
+      person.emails = document.getElementById("textEmails").value.split(",");
+      person.telephones = document
+        .getElementById("textTelephone_numbers")
+        .value.split(",");
+      person.birthday = document.getElementById("textBirthday").value;
+      person.profession = document.getElementById("textProfession").value;
+      view.textContent = "Os dados do usuário foram atualizados com sucesso!";
+    } else {
+      view.innerHTML = "O CPF informado não consta na base de dados atual.";
     }
+  } catch (e) {
+    console.log(
+      "Ocorreu um erro ao tentar atualizar os dados do usuário especificado."
+    );
+    console.log(e.name); // Imprime o nome do erro no console.
+    console.log(e.message); // Imprime a mensagem do erro no console.
+  } finally {
+    // Limpa a área de visualização após 45000 milissegundos.
+    setTimeout(cleaner, 45000);
+  }
 }
 
 // Função que exclui uma pessoa da lista pelo CPF.
 function delete_people() {
-    let CPF = document.getElementById('textCPF').value;
-    try {
-        let match = find_people(CPF);
+  let CPF = document.getElementById("textCPF").value;
+  try {
+    let match = find_people(CPF);
 
-        if (match > -1) {
-            const view = document.getElementsByClassName('view')[0];
-            view.textContent = '';
+    if (match > -1) {
+      const view = document.getElementsByClassName("view")[0];
+      view.textContent = "";
 
-            // Remove a pessoa da lista filtrando pelo CPF.
-            peoples = peoples.filter(element => element.CPF !== CPF);
-            saveData_peoples();
-            view.textContent = 'O usuário foi excluído com sucesso!';
-        } else {
-            view.innerHTML = 'O CPF informado não consta na base de dados atual.';
-        }
-    } catch (e) {
-        console.log("Ocorreu um erro ao tentar excluir os dados do usuário especificado.");
-        console.log(e.name); // Imprime o nome do erro no console.
-        console.log(e.message); // Imprime a mensagem do erro no console.
-    } finally {
-        // Limpa a área de visualização após 45000 milissegundos.
-        setTimeout(cleaner, 45000);
+      // Remove a pessoa da lista filtrando pelo CPF.
+      peoples = peoples.filter((element) => element.CPF !== CPF);
+      saveData_peoples();
+      view.textContent = "O usuário foi excluído com sucesso!";
+    } else {
+      view.innerHTML = "O CPF informado não consta na base de dados atual.";
     }
+  } catch (e) {
+    console.log(
+      "Ocorreu um erro ao tentar excluir os dados do usuário especificado."
+    );
+    console.log(e.name); // Imprime o nome do erro no console.
+    console.log(e.message); // Imprime a mensagem do erro no console.
+  } finally {
+    // Limpa a área de visualização após 45000 milissegundos.
+    setTimeout(cleaner, 45000);
+  }
 }
